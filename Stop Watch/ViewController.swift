65b8b647d2b5ff186621e270374a8d0859438b34
Anuja Tupe <anujatupe2012@gmail.com>
2015-04-07 22:19:28 -0700

    Hitting on pause button stops the timer but doesn't set the counter to zero. So when user taps on play it starts where it left

--- Stop Watch/ViewController.swift
+++ Stop Watch/ViewController.swift
@@ -31,6 +31,10 @@ class ViewController: UIViewController {
         counter = 0
     }
     
+    @IBAction func pauseTimer(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
